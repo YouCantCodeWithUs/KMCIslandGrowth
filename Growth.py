61e2f87b4f11769252379fcfa7db4bc63da5d6e8
Kai Wells <support@kaiwells.me>
2015-04-12 13:26:32 -0500

    Initial Python Commit
    Outline of a few functions

---
+++ Growth.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2.7
+# -*- coding: utf-8 -*-
+
+import numpy as np
+
+def InitPositionsFCC(N, a):
+	'''
+	Initialize `N` atoms in an FCC lattice with orientation (111) and spacing `a`.
+	Return the positions of these atoms.
+	'''
+
+def AdatomSurfaceEnergy(adatom, substrate):
+	'''
+	Returns the potential energy of the `adatom` due to the `substrate` atoms
+	'''
+
+def AdatomSurface
